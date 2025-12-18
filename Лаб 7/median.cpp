9e0dae3bb686349e1e450416460f39eaa23f8cc0
vlvlvl182 <benasvilit@gmail.com>
2025-12-18 04:06:15 +0300

    Update median.cpp

--- Лаб 7/median.cpp
+++ Лаб 7/median.cpp
@@ -1,34 +1,30 @@
+#include "pch.h"
 #include "median.h"
-#include <vector>
 #include <algorithm>
+#include <cmath>
+#include <ctime>
+MEDIANLIB_API double calculate_median(const double* arr, int size) {
+   clock_t start_time = clock();
+   if (size <= 0 || arr == nullptr) {
 
-MEDIAN_API double array_median(const double* arr, int size)
-{
-    if (size <= 0 || arr == nullptr) {
-        return 0.0;
-    }
+       return 0.0;
+   }
+   double* data = new double[size];
+   std::copy(arr, arr + size, data);
+   std::sort(data, data + size);
+   double median = 0.0;
+   int mid = size / 2;
 
-    std::vector<double> data(arr, arr + size);
-    std::sort(data.begin(), data.end());
+   if (size % 2 == 0) {
 
-    if (size % 2 == 1) {
-        return data[size / 2];
-    }
-    else {
-        return (data[size / 2 - 1] + data[size / 2]) / 2.0;
-    }
-}
+       median = (data[mid - 1] + data[mid]) / 2.0;
+   }
+   else {
+       median = data[mid];
+   }
+   delete[] data;
+   clock_t end_time = clock();
+   double elapsed_time = static_cast<double>(end_time - start_time) / CLOCKS_PER_SEC;
+   return elapsed_time;
 
-MEDIAN_API double array_median_n_times(const double* arr, int size, int iterations)
-{
-    if (iterations <= 0) {
-        return 0.0;
-    }
-
-    double total = 0.0;
-    for (int i = 0; i < iterations; i++) {
-        total += array_median(arr, size);
-    }
-
-    return total;
 }
