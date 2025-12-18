6ea92e07be0b1fdb8d330e6dea44735da69d1014
vlvlvl182 <benasvilit@gmail.com>
2025-12-18 04:00:50 +0300

    Update median.h

--- Лаб 7/median.h
+++ Лаб 7/median.h
@@ -1,16 +1,14 @@
 #pragma once
-
-#ifdef _WIN32
-#ifdef MEDIAN_EXPORTS
-#define MEDIAN_API __declspec(dllexport)
+#ifdef MEDIANLIB_EXPORTS
+#define MEDIANLIB_API __declspec(dllexport)
 #else
-#define MEDIAN_API __declspec(dllimport)
+#define MEDIANLIB_API __declspec(dllimport)
 #endif
-#else
-#define MEDIAN_API
-#endif
-
 extern "C" {
-    MEDIAN_API double array_median(const double* arr, int size);
-    MEDIAN_API double array_median_n_times(const double* arr, int size, int iterations);
+   MEDIANLIB_API double calculate_median(const double* arr, int size);
+
 }
+
+ 
+
+
